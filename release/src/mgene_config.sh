76f3b97bd2df85d92bc1e105ac68b7cd2903eb11
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-07-01 11:05:48 +0000

    remove .o files from repository
    Former-commit-id: 24f7572b9f500e605a039af47fff172766f287d3

--- release/src/mgene_config.sh
+++ release/src/mgene_config.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 export INTERPRETER=octave
-export OCTAVE_BIN_PATH=/usr/bin/octave
+export OCTAVE_BIN_PATH=/home/galaxy/software/octave-3.2.3-64-new/bin/octave
 export OCTAVE_OPTS=
-export MKOCT_BIN=/usr/bin/mkoctfile
+export MKOCT_BIN=/home/galaxy/software/octave-3.2.3-64-new/bin/mkoctfile
 export MEX_BIN=echo
-export MGENE_SRC_PATH=/fml/ag-raetsch/home/jonas/svn/releases/mGeneToolbox-0.2.0/release/src
-export SHOGUN_PATH=/fml/ag-raetsch/home/jonas/svn/releases/mGeneToolbox-0.2.0/release/src/shogun
-export OPTIMIZER=cplex
-export ILOG_LICENSE_FILE=/fml/ag-raetsch/share/software/ilog/licenses/access-587380.ilm
-export CPLEX_INCL=/fml/ag-raetsch/share/software/ilog/cplex91/include/ilcplex/
-export CPLEX_LIB=/fml/ag-raetsch/share/software/ilog/cplex91/lib/x86-64_sles9.0_3.3/static_pic/
-export OPTIMIZER_PATH=/usr/local/bin/cplex
+export MGENE_SRC_PATH=/home/galaxy/svn/releases/mGeneToolbox-0.2.0/release/src
+export SHOGUN_PATH=/home/galaxy/svn/releases/mGeneToolbox-0.2.0/release/src/shogun
